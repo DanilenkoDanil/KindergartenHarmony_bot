1649a72ff2d67ac783ecba00812f32687281fb9a
DanilenkoDanil <69428366+DanilenkoDanil@users.noreply.github.com>
2020-11-29 21:13:32 +0200

    Убрал лишний код
    Убрал отправку айди пользователя, так как она уже не нужна.

--- bot_01.py
+++ bot_01.py
@@ -13,7 +13,6 @@ def control(message):
     if len(message.text) > 55 and message.from_user.id != 1028496717 and message.from_user.id != 763070224:
         bot.forward_message(-1001393620851, message.chat.id, message.message_id)
         bot.delete_message(message.chat.id, message.message_id)
-        bot.send_message(-1001393620851, message.from_user.id)
 
 
 while True:
